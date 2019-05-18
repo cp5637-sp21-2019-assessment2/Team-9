fc5b050e5e9d2b7a2c8f6f448952305c45b10d67
PavanSingaraju <50400317+PavanSingaraju@users.noreply.github.com>
2019-05-18 11:01:49 +0530

    Update customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -50,6 +50,6 @@ function team9_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function team9_customize_preview_js() {
-	wp_enqueue_script( 'team9-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'team9-customizer', get_template_directory_uri() . '/dist/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'team9_customize_preview_js' );
